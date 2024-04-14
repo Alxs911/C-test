30f4f1eca9cc2652218909ae4b2dfb78b4904845
alxs <alxsbobryshev@gmail.com>
2024-04-14 21:50:13 +0300

    add sortirovka vstavkoi

--- main.cpp
+++ main.cpp
@@ -12,11 +12,23 @@
 using namespace std;
 
 
-int Hello()
 
 
 
 int main() {
-    int hello[] {1,6,2,8,3,5,12,23};
+    int n = 8;
+    int v[] {1,6,2,8,3,5,12,23};
+    for (int i = 1; i < n; i++){
+        for (int j = i; (j > 0) && (v[j] < v[j - 1]); j--) {
+            int c;
+
+            c = v[j];
+            v[j] = v[j - 1];
+            v[j - 1] = c;
+        }
+    }
+    for (int i = 0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
     return 0;
 }
