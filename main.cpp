ace29b75a6f8fcc3e71e152799aad29cbd13c2d8
Alxs911 <167024958+Alxs911@users.noreply.github.com>
2024-04-14 21:38:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+int Hello()
+
+
+
+int main() {
+    int hello[] {1,6,2,8,3,5,12,23};
+    return 0;
+}
